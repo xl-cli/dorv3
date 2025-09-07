c867d1005b176b2218fc5d8ac596959898c051bb
flyxt <falemailfal@gmail.com>
2025-09-07 16:36:46 +0700

    add rebuy package

--- my_package.py
+++ my_package.py
@@ -1,5 +1,5 @@
 from api_request import get_package, send_api_request
-from ui import clear_screen, pause
+from ui import clear_screen, pause, show_package_details
 from auth_helper import AuthInstance
 
 # Fetch my packages
@@ -35,6 +35,7 @@ def fetch_my_packages():
     print("===============================")
     print("My Packages")
     print("===============================")
+    my_packages =[]
     num = 1
     for quota in quotas:
         quota_code = quota["quota_code"] # Can be used as option_code
@@ -54,6 +55,25 @@ def fetch_my_packages():
         print(f"Family Code: {family_code}")
         print(f"Group Code: {group_code}")
         print("===============================")
+        
+        my_packages.append({
+            "number": num,
+            "quota_code": quota_code,
+        })
+        
+        print("Rebuy package? Input package number to rebuy, or '00' to back.")
+        choice = input("Choice: ")
+        if choice == "00":
+            return None
+        selected_pkg = next((pkg for pkg in my_packages if str(pkg["number"]) == choice), None)
+        
+        if not selected_pkg:
+            print("Paket tidak ditemukan. Silakan masukan nomor yang benar.")
+            continue
+        
+        is_done = show_package_details(api_key, tokens, selected_pkg["quota_code"])
+        if is_done:
+            return None
         
         num += 1
         
